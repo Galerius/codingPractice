1c75eaf38024dfd9bc6eef28e7990b3901089be2
adam <akledyard@gmail.com>
2017-02-28 02:01:10 +0000

    Updated to check all time of parens

--- src/balancedParens.java
+++ src/balancedParens.java
@@ -3,8 +3,9 @@ import java.util.*;
 public class balancedParens {
     
     public static void main(String[] args){
-        String[] testStrings = new String[]{"[]","[]]"};
+        String[] testStrings = new String[]{"[]","[]]","{","{[]}()","(())","[()]","{[}","foo(bar)","foo(bar[i)"};
         for( int i = 0; i < testStrings.length; i++){
+            System.out.println("String: " + testStrings[i]);
             System.out.println("Result: " + isBalanced(testStrings[i]));
         }
     }
@@ -13,26 +14,34 @@ public class balancedParens {
         LinkedList<Character> parenStack = new LinkedList<Character>();
         for(int i = 0; i < str.length(); i++){
             char currentParen = str.charAt(i);
-            if(currentParen == '[' || currentParen == '('){
+            //System.out.println(currentParen);
+            char topOfStack;
+            if(currentParen == '[' || currentParen == '(' || currentParen == '{'){
                 parenStack.addFirst(currentParen);
             }
             //System.out.println(currentParen);
-            else if(currentParen == ']' || current == ')'){
-                if(topOfStack.isEmpty()){
+            else if(currentParen == ']' || currentParen == ')' || currentParen == '}'){
+                if(parenStack.isEmpty()){
                     return Integer.toString(i);
                 }
-                char topOfStack = parenStack.pop();
-                if(topOfStack != '[') {
+                else{
+                    topOfStack = parenStack.pop();    
+                }
+                
+                if(currentParen == ']' && topOfStack != '[') {
+                    return Integer.toString(i);
+                }
+                else if(currentParen == ')' && topOfStack != '('){
+                    return Integer.toString(i);
+                }
+                else if(currentParen == '}' && topOfStack != '{'){
                     return Integer.toString(i);
                 }
             }
-             else if(currentParen == ')'){
-                 char topOfStack = parenStack.pop();
-                if(topOfStack != '('){
-                    return Integer.toString(i);
-                } 
-             }
-
+        }
+        //System.out.println(Arrays.toString(parenStack.toArray()));
+        if( !parenStack.isEmpty()){
+            return Integer.toString(str.length());
         }
         return "SUCCESS";
     }
